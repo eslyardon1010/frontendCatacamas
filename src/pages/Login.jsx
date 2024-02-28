cb7f7a2a87f2edb9a40043695373daec1c91449e
Esly Ardon <77656790+eslyardon1010@users.noreply.github.com>
2024-02-28 11:38:12 -0600

    Add files via upload

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -113,12 +113,51 @@ const Login = () => {
 	}
 
 	return (
-		<div>
-			<h1>
-				HOLA MUNDO
-			</h1>
+		<div className='bg-[#F5F8FE] w-full min-h-[100vh] flex justify-center items-center select-none py-16'>
+			<div className='w-[600px] h-auto bg-white py-[50px] px-[100px] shadow-lg'>
+				<div className='w-full flex justify-center mb-3'>
+					<Link to='/'>
+						<div className='w-[200px]'>
+							<img src={Logo} alt='Logo' />
+						</div>
+					</Link>
+				</div>
+				<h1 className='text-lg font-bold mb-5'>Login</h1>
+				<form action='' className='flex flex-col gap-y-5'>
+					{/* Email */}
+					<Input
+						label='Correo electrónico'
+						type='email'
+						name='correo'
+						id='correo'
+						placeholder='ejemplo@dominio'
+						value={email}
+						onChange={e => setEmail(e.target.value)}
+					/>
+
+					{/* Pasword */}
+					<Input
+						label='Contraseña'
+						type='password'
+						name='password'
+						id='password'
+						placeholder='*****'
+						value={password}
+						onChange={e => setPassword(e.target.value)}
+					/>
+					<div className='mt-2'>
+						<button
+							type='submit'
+							onClick={e => onClickLogin(e)}
+							className='w-full py-3 rounded-full bg-[var(--colorPrimario)] text-base font-semibold text-white hover:opacity-80 duration-200 cursor-pointer shadow-lg mb-3'
+						>
+							Login
+						</button>
+					
+					</div>
+				</form>
+			</div>
 		</div>
-		/* */
 	)
 }
 
